a31d674db4da9eb0638099c45889e1dd2624fc22
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-06-14 15:04:01 +0300

    My commit message

--- javascript/updateProject.js
+++ javascript/updateProject.js
@@ -113,15 +113,25 @@ document.getElementById("ixe5wc").addEventListener("click", event => {
 function initializearrayib5cm3(data) {
   arrayib5cm3 = data.map(item => ({
     value: item._id,
-    liValue: item[' ']
+    liValue: item['wName']
   }));
 }
-document.getElementById('i7w1n').onclick = (event) => {
+document.addEventListener('alignpWorkpackage', function(e) {
+  const advanceSelect = document.getElementById('im1p3y');
+  const selectedElement = advanceSelect.getAttribute('selected-element');
+  if (!selectedElement) return;
+  [...advanceSelect.querySelectorAll("[annotationname]")].forEach(
+    optionElement => {
+      if (optionElement.value === selectedElement)
+        optionElement.setAttribute("selected", true);
+    }
+  );
+});document.getElementById('i7w1n').onclick = (event) => {
     event.preventDefault();
     let projectId = window.location.pathname.replace('/updateProject/','');let project = new TempApi.Project();project['pImage'] = {
         data: document.querySelector("[annotationname = 'pImage']").getAttribute("data-image-base64") !== null ? document.querySelector("[annotationname = 'pImage']").getAttribute("data-image-base64") : document.querySelector("[annotationname = 'pImage']").src,
         name: document.querySelector("[annotationname = 'pImage']").getAttribute("name")
-      };project['pTitle'] = document.querySelector("[annotationname = 'pTitle']").value;project['pStart'] = document.querySelector("[annotationname = 'pStart']").value;project['pWebsite'] = document.querySelector("[annotationname = 'pWebsite']").value;project['pEnd'] = document.querySelector("[annotationname = 'pEnd']").value;project['pDuration'] = document.querySelector("[annotationname = 'pDuration']").value;project['pGA'] = document.querySelector("[annotationname = 'pGA']").value;project['pAbstract'] = document.querySelector("[annotationname = 'pAbstract']").value; let opts = {project};apiProjectApi.updateproject( projectId, opts, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); 
+      };project['pTitle'] = document.querySelector("[annotationname = 'pTitle']").value;project['pStart'] = document.querySelector("[annotationname = 'pStart']").value;project['pWebsite'] = document.querySelector("[annotationname = 'pWebsite']").value;project['pEnd'] = document.querySelector("[annotationname = 'pEnd']").value;project['pDuration'] = document.querySelector("[annotationname = 'pDuration']").value;project['pGA'] = document.querySelector("[annotationname = 'pGA']").value;project['pAbstract'] = document.querySelector("[annotationname = 'pAbstract']").value;project['pWorkpackage'] = arrayib5cm3.map(item => item.value); let opts = {project};apiProjectApi.updateproject( projectId, opts, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); 
       if(response.body.query.pImage !== undefined){
 
         if(document.querySelector('[annotationname = pImage]').getAttribute('type') === 'file'){
@@ -132,7 +142,7 @@ document.getElementById('i7w1n').onclick = (event) => {
         }
         document.querySelector('[annotationname = pImage]').name = response.body.query.pImage.name;
       }
-      document.querySelector('[annotationname = pTitle]').value = response.body.query.pTitle ;document.querySelector('[annotationname = pStart]').value = response.body.query.pStart ;document.querySelector('[annotationname = pWebsite]').value = response.body.query.pWebsite ;document.querySelector('[annotationname = pEnd]').value = response.body.query.pEnd ;document.querySelector('[annotationname = pDuration]').value = response.body.query.pDuration ;document.querySelector('[annotationname = pGA]').value = response.body.query.pGA ;document.querySelector('[annotationname = pAbstract]').value = response.body.query.pAbstract ;}});};window.onload = () => {let projectId = window.location.pathname.replace('/updateProject/','');apiProjectApi.getproject( projectId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); const map = new Map();try { document.querySelector('[annotationname = pTitle]').value = response.body.query.pTitle; } catch (e) { console.log(e) };try { 
+      document.querySelector('[annotationname = pTitle]').value = response.body.query.pTitle ;document.querySelector('[annotationname = pStart]').value = response.body.query.pStart ;document.querySelector('[annotationname = pWebsite]').value = response.body.query.pWebsite ;document.querySelector('[annotationname = pEnd]').value = response.body.query.pEnd ;document.querySelector('[annotationname = pDuration]').value = response.body.query.pDuration ;document.querySelector('[annotationname = pGA]').value = response.body.query.pGA ;document.querySelector('[annotationname = pAbstract]').value = response.body.query.pAbstract ;initializearrayib5cm3(response.body.query.pWorkpackage|| []) ; }});};window.onload = () => {let projectId = window.location.pathname.replace('/updateProject/','');apiProjectApi.getproject( projectId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); const map = new Map();try { document.querySelector('[annotationname = pTitle]').value = response.body.query.pTitle; } catch (e) { console.log(e) };try { 
       if(response.body.query.pImage !== undefined){
         if(document.querySelector('[annotationname = pImage]').getAttribute('type') === 'file'){
           document.querySelector('[annotationname = pImage]').setAttribute('data-image-base64',response.body.query.pImage.data);
@@ -148,4 +158,91 @@ document.getElementById('i7w1n').onclick = (event) => {
         }
         document.querySelector('[annotationname = pImage]').name = response.body.query.pImage.name ;
       }
-       } catch (e) { console.log(e) };try { document.querySelector('[annotationname = pWebsite]').value = response.body.query.pWebsite; } catch (e) { console.log(e) };try { document.querySelector('[annotationname = pStart]').value = response.body.query.pStart; } catch (e) { console.log(e) };try { document.querySelector('[annotationname = pEnd]').value = response.body.query.pEnd; } catch (e) { console.log(e) };try { document.querySelector('[annotationname = pDuration]').value = response.body.query.pDuration; } catch (e) { console.log(e) };try { document.querySelector('[annotationname = pGA]').value = response.body.query.pGA; } catch (e) { console.log(e) };try { document.querySelector('[annotationname = pAbstract]').value = response.body.query.pAbstract; } catch (e) { console.log(e) };window.localStorage.setItem('data', JSON.stringify(Array.from(map.entries())));}});};
+       } catch (e) { console.log(e) };try { document.querySelector('[annotationname = pWebsite]').value = response.body.query.pWebsite; } catch (e) { console.log(e) };try { document.querySelector('[annotationname = pStart]').value = response.body.query.pStart; } catch (e) { console.log(e) };try { document.querySelector('[annotationname = pEnd]').value = response.body.query.pEnd; } catch (e) { console.log(e) };try { document.querySelector('[annotationname = pDuration]').value = response.body.query.pDuration; } catch (e) { console.log(e) };try { document.querySelector('[annotationname = pGA]').value = response.body.query.pGA; } catch (e) { console.log(e) };try { document.querySelector('[annotationname = pAbstract]').value = response.body.query.pAbstract; } catch (e) { console.log(e) };try { 
+        document.querySelector('[annotationname = pWorkpackage]').setAttribute('selected-element',response.body.query.pWorkpackage.wName);document.dispatchEvent(new Event("alignpWorkpackage"));
+        const insideSubdocument = document.querySelector("[annotationname = 'pWorkpackage']");
+        if (insideSubdocument !==null) {
+          const tableData = response.body.query.pWorkpackage;
+    initializearrayib5cm3(tableData); 
+ refreshULixe5wc();
+    const tableDataElement = insideSubdocument.querySelectorAll("[dataitem='true']");
+    tableData.forEach((data,index) => {
+      if(tableDataElement.length < index) {
+        return;
+      }
+       try {
+      const attributeSubdocumentElement = tableDataElement[
+        index
+      ].querySelector("[annotationname = 'wName']");
+      if (attributeSubdocumentElement !== null) {
+        attributeSubdocumentElement.textContent = tableData[tableData.length - index -1].wName;
+      }
+    }
+    catch(e) {console.log(e);};
+      
+      map.set(
+        tableDataElement[index].getAttribute("id"),
+        tableData[tableData.length - index -1]
+      );
+    
+    });
+    
+      [...tableDataElement].forEach((element, index) => {
+        if (index >= tableData.length) {
+          tableDataElement[index].style.display = "none";
+        }
+        else {
+          tableDataElement[index].style.display = "";
+        }
+      });
+    
+    
+        }
+      if(response.body.query.pWorkpackage._id){
+        map.set(
+          document.querySelector(
+            "[annotationname = 'pWorkpackage']"
+          ).getAttribute("id"),
+          response.body.query.pWorkpackage
+        );
+      }
+     } catch (e) { console.log(e) };try { 
+        document.querySelector('[annotationname = pDeliverable]').setAttribute('selected-element',response.body.query.pDeliverable.undefined);
+        const insideSubdocument = document.querySelector("[annotationname = 'pDeliverable']");
+        if (insideSubdocument !==null) {
+          const tableData = response.body.query.pDeliverable;
+    
+    const tableDataElement = insideSubdocument.querySelectorAll("[dataitem='true']");
+    tableData.forEach((data,index) => {
+      if(tableDataElement.length < index) {
+        return;
+      }
+      
+      
+      map.set(
+        tableDataElement[index].getAttribute("id"),
+        tableData[tableData.length - index -1]
+      );
+    
+    });
+    
+      [...tableDataElement].forEach((element, index) => {
+        if (index >= tableData.length) {
+          tableDataElement[index].style.display = "none";
+        }
+        else {
+          tableDataElement[index].style.display = "";
+        }
+      });
+    
+    
+        }
+      if(response.body.query.pDeliverable._id){
+        map.set(
+          document.querySelector(
+            "[annotationname = 'pDeliverable']"
+          ).getAttribute("id"),
+          response.body.query.pDeliverable
+        );
+      }
+     } catch (e) { console.log(e) };window.localStorage.setItem('data', JSON.stringify(Array.from(map.entries())));}});};
--- javascript/updateTask.js
+++ javascript/updateTask.js
@@ -1,4 +1,4 @@
-document.getElementById('iskq').onclick = (event) => {
+let apiTaskApi = new TempApi.TaskApi();import TempApi from '../src/index';document.getElementById('iskq').onclick = (event) => {
     event.preventDefault();
     {  location.href= '/' ;}};document.getElementById('i62kh').onclick = (event) => {
     event.preventDefault();
@@ -22,4 +22,6 @@ document.getElementById('iskq').onclick = (event) => {
       function () { $("#datepicker").datepicker({format: 'dd-mm-yyyy'}); }
     );$(
       function () { $("#datepicker-2").datepicker({format: 'dd-mm-yyyy'}); }
-    );window.onload = () => {};
+    );document.getElementById('iwh0h').onclick = (event) => {
+    event.preventDefault();
+    let taskId = window.location.pathname.replace('/updateTask/','');let task = new TempApi.Task();task['tName'] = document.querySelector("[annotationname = 'tName']").value;task['tStart'] = document.querySelector("[annotationname = 'tStart']").value;task['tEnd'] = document.querySelector("[annotationname = 'tEnd']").value;task['tPMs'] = document.querySelector("[annotationname = 'tPMs']").value;task['tDescription'] = document.querySelector("[annotationname = 'tDescription']").value; let opts = {task};apiTaskApi.updatetask( taskId, opts, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); document.querySelector('[annotationname = tName]').value = response.body.query.tName ;document.querySelector('[annotationname = tStart]').value = response.body.query.tStart ;document.querySelector('[annotationname = tEnd]').value = response.body.query.tEnd ;document.querySelector('[annotationname = tPMs]').value = response.body.query.tPMs ;document.querySelector('[annotationname = tDescription]').value = response.body.query.tDescription ;}});};window.onload = () => {let taskId = window.location.pathname.replace('/updateTask/','');apiTaskApi.gettask( taskId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); try { document.querySelector('[annotationname = tName]').value = response.body.query.tName; } catch (e) { console.log(e) };try { document.querySelector('[annotationname = tStart]').value = response.body.query.tStart; } catch (e) { console.log(e) };try { document.querySelector('[annotationname = tEnd]').value = response.body.query.tEnd; } catch (e) { console.log(e) };try { document.querySelector('[annotationname = tPMs]').value = response.body.query.tPMs; } catch (e) { console.log(e) };try { document.querySelector('[annotationname = tDescription]').value = response.body.query.tDescription; } catch (e) { console.log(e) };}});};
